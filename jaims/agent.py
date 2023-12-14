cd9342c08753340b52bb778e2571c1a330c01f84
Marco Musella <mar.musella@gmail.com>
2023-12-14 14:49:30 +0100

    fixes

--- jaims/agent.py
+++ jaims/agent.py
@@ -173,8 +173,7 @@ class JAImsAgent:
                     and message_delta.tool_calls
                 ):
                     print(
-                        # type: ignore
-                        message_delta.tool_calls[0].function.arguments,
+                        message_delta.tool_calls[0].function.arguments,  # type: ignore
                         flush=True,
                         end="",
                     )
@@ -382,8 +381,7 @@ class JAImsAgent:
             return new_delta
 
         if new_delta.content:
-            accumulator.content = (
-                accumulator.content or "") + new_delta.content
+            accumulator.content = (accumulator.content or "") + new_delta.content
         if new_delta.role:
             accumulator.role = (accumulator.role or "") + new_delta.role
         if new_delta.tool_calls:
